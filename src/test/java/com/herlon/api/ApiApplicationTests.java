f9b6a48c132bccd7c05289dad4532d16cd9d5e31
HerlonCortez <noklyn@gmail.com>
2025-03-19 08:02:01 -0300

    Primeiro commit

---
+++ src/test/java/com/herlon/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herlon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
