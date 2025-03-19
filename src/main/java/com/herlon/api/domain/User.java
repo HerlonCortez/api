c127b59d952c390f559f42f3068b5f60e13de8d8
HerlonCortez <noklyn@gmail.com>
2025-03-19 08:09:20 -0300

    Commit da calsse usuario

---
+++ src/main/java/com/herlon/api/domain/User.java
@@ -0,0 +1,24 @@
+package com.herlon.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
